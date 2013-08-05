123c0e0abe3a5e6af91fb4ae6dec31104aee8bb1
Dmitry Galinsky <dima.exe@gmail.com>
2013-08-05 17:59:03 +0400

    add port to login task

--- lib/capistrano_evrone_recipes/recipes/login.rb
+++ lib/capistrano_evrone_recipes/recipes/login.rb
@@ -1,5 +1,6 @@
 desc "Login to remote host"
 task :login do
   hostname = find_servers_for_task(current_task).first
-  exec "ssh -A -l #{user} #{hostname} -t 'cd #{current_path} && ${SHELL} -l'"
+  port = hostname.port || fetch(:port, 22)
+  exec "ssh -A -p #{port} -l #{user} #{hostname} -t 'cd #{current_path} && ${SHELL} -l'"
 end
